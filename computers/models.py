72b2b5d794e628b72b6c19442955e9a944bc7918
Matthildur <matthildura19@ru.is>
2020-05-05 15:26:08 +0000

    module

--- computers/models.py
+++ computers/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class computer_catergory(models.Model):
+class computer_category(models.Model):
     name = models.CharField(max_length=255)
 
 class computers(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
-    category = models.ForeignKey(computer_catergory, on_delete=models.CASCADE)
+    category = models.ForeignKey(computer_category, on_delete=models.CASCADE)
     price = models.FloatField()
     on_sale = models.BooleanField()
 
