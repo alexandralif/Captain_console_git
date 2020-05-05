14d0cdff9d1f4385ff0137f49b38e653c10e81c0
Matthildur <matthildura19@ru.is>
2020-05-05 12:32:15 +0000

    created models

--- computers/models.py
+++ computers/models.py
@@ -3,23 +3,24 @@ from django.db import models
 # Create your models here.
 from django.db.models import ForeignKey
 
-from games.models import Games
+from games.models import games
 
-
-class computer_catergory(models.Model):
+class computer_category(models.Model):
     name = models.CharField(max_length=255)
 
 class computers(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
-    category = models.ForeignKey(computer_catergory, on_delete=models.CASCADE)
+    category = models.ForeignKey(computer_category, on_delete=models.CASCADE)
     price = models.FloatField()
     on_sale = models.BooleanField()
-    games = models.ForeignKey(Games, on_delete=models.CASCADE)
+    games = models.ForeignKey(games, on_delete=models.CASCADE)
 
 class computer_image(models.Model):
     image = models.CharField(max_length=999)
     computers = models.ForeignKey(computers, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image
 
 
 
