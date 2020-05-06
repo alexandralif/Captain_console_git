5355f0e4709703a2cd02f496c105b3de9569316a
berthamaria <54938563+berthamaria@users.noreply.github.com>
2020-05-06 14:47:23 +0000

    lagaði merge

--- games/views.py
+++ games/views.py
@@ -3,12 +3,7 @@ from games.models import games
 
 
 def index(request):
-<<<<<<< HEAD
-    return render(request, "games/index.html")
-
-
-=======
     return render(request, "games/index.html", {
         'games': games.objects.all()
     })
->>>>>>> 4a440566964b23a05c86190a3235720a99caf2fa
+
