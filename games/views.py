248f0d44d739c04c28d4eb5ab989bd2cc77d2a81
berthamaria <54938563+berthamaria@users.noreply.github.com>
2020-05-06 15:57:56 +0000

    lagaði confligt

--- games/views.py
+++ games/views.py
@@ -7,10 +7,8 @@ def index(request):
         'games': games.objects.all()
     })
 
-<<<<<<< HEAD
 def get_game_by_id(request,id):
     return render(request, 'games/game_details.html', {
         'computers': get_object_or_404(games, pk=id)
     })
-=======
->>>>>>> 60cc43c89f015f5c136754f4cbdb7168176ae841
+
