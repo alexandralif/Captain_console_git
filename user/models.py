c653f517f05c98385d697cf97e8fce1d4c3f090f
Matthildur <matthildura19@ru.is>
2020-05-11 14:42:13 +0000

    reyna við mínar síður

--- user/models.py
+++ user/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+class account(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    account_img = models.CharField(max_length=999,default='https://icons8.com/icon/82751/user')
