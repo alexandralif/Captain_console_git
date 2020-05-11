f1b3aac00f014fe95da43986d5af1d8165123d43
berthamaria <54938563+berthamaria@users.noreply.github.com>
2020-05-11 11:27:38 +0000

    karfan virkar ekki næs

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,9 +6,12 @@ from computers.models import computers
 from front_page.models import products
 
 
+
 class Cart(models.Model):
-    products = models.ManyToManyField(products,  null=True, blank=True)
-    total_price = models.FloatField()
+    products = models.ForeignKey(products, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    quantity = models.FloatField()
+
 
 
 
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from cart.models import Cart
+from front_page.models import products
 
+
+@login_required
 def index(request):
     context = {'cart': Cart.objects.all().order_by('products')}
     return render(request, 'cart/index.html', context)
 
+#@login_required
+def add_to_cart(request, id):
+    print('hæhæ')
+    return render(request, 'front_page/product_details.html', {
+        'products': get_object_or_404(products, pk=id)
+    })
 
 # Create your views here.
