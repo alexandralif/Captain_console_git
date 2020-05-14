c5774feac6e19c29e824ac595bc473429d5821f6
berthamaria <54938563+berthamaria@users.noreply.github.com>
2020-05-14 17:02:48 +0000

    reyna að birta uppl

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout',LogoutView.as_view(next_page='login'),name='logout'),
     path('my_account',LoginView.as_view(template_name='user/my_account.html'),name='my_account'),
     #path('create_user', LoginView.as_view(template_name='user/create_user.html'), name="create_user")
-    path('create_user', views.create_user, name="create_user")
+    path('create_user', views.create_user, name="create_user"),
+    path('my_account',views.review_info, name='user-review_info'),
 ]
 
