72c723b7d67df7a10d81d49d5addefe2b2b91647
berthamaria <54938563+berthamaria@users.noreply.github.com>
2020-05-14 00:30:14 +0000

    kommenta fullt

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from products.models import products
 from django.http import JsonResponse
 
 def index(request):
+    '''this function allows the user to search a product by a name!'''
     if 'search_filter' in request.GET:
         search_filter = request.GET['search_filter']
         product = [{
@@ -23,34 +24,42 @@ def get_product_by_id(request,id):
     })
 
 def get_all_games(request):
+    '''this is a function that groups all the games in one'''
     context = {'products': products.objects.filter(type_id=2)}
     return render(request, "products/index.html", context)
 
 def get_all_computers(request):
+    '''this is a function that groups all the computers in one'''
     context = {'products': products.objects.filter(type_id=1)}
     return render(request, "products/index.html", context)
 
 def ordered_by_price(request):
+    '''this is a function that orders the products by price'''
     context = {'products': products.objects.all().order_by('price')}
     return render(request, "products/index.html", context)
 
 
 def ordered_by_name(request):
+    '''this is product that orders the products by name'''
     context = {'products': products.objects.all().order_by('name')}
     return render(request, "products/index.html", context)
 
 def get_nintendo_products(request):
+    '''this function get's all products that have the same manufacturer'''
     context = {'products': products.objects.filter(category_id=1)}
     return render(request, "products/index.html", context)
 
 def get_gameboy_products(request):
+    '''this function get's all products that have the same manufacturer'''
     context = {'products': products.objects.filter(category_id=2)}
     return render(request, "products/index.html", context)
 
 def get_playstation_products(request):
+    '''this function get's all products that have the same manufacturer'''
     context = {'products': products.objects.filter(category_id=3)}
     return render(request, "products/index.html", context)
 
 def get_xbox_products(request):
+    '''this function get's all products that have the same manufacturer'''
     context = {'products': products.objects.filter(category_id=4)}
     return render(request, "products/index.html", context)
