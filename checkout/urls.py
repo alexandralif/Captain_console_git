ee3881fd5ddffe3f406b8754adbf9059094ccba5
Matthildur <matthildura19@ru.is>
2020-05-14 16:13:55 +0000

    review

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.index,name='checkout-index'),
     path('add_personal_info/',views.add_personal_info, name='checkout-add_personal_info'),
-    path('payment/',views.payment,name='checkout-payment'),
+    path('payment/',views.payment_info,name='checkout-payment'),
     path('review/',views.review, name='checkout-review'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,29 +1,58 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect,get_object_or_404
+from checkout.models import personal_info
+from cart.models import Cart
+from checkout.forms.payment_info import PaymentForm
+from checkout.models import payment
 from checkout.forms.personal_info_form import PersonalForm
 
 
-#def index(request):
-#    context = {}
 
- #   return render(request, 'checkout/index.html', context)
+
 
 def add_personal_info(request):
+    #u = User.objects.filter(pk=request.user.id).first()
+    #pers_info = personal_info.objects.filter(user=u).first()
+    profile = personal_info.objects.filter(user=request.user).first()
     if request.method == 'POST':
-        form = PersonalForm(data=request.POST)
+        form = PersonalForm(data=request.POST,instance=profile)
         if form.is_valid():
-            form.save()
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            #return redirect('checkout/payment')
     else:
         form = PersonalForm()
     return render(request, 'checkout/add_personal_info.html', {
         'form': form
     })
-def payment(request):
-    context = {}
-    return render(request, 'checkout/payment.html', context)
+def payment_info(request):
+    profile = payment.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        form = PaymentForm(data=request.POST, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            # return redirect('checkout/payment')
+    else:
+        form = PaymentForm()
+
+    return render(request, 'checkout/payment.html', {
+        'form': form
+    })
+
 
 def review(request):
-    context = {}
-    return render(request, 'checkout/review.html', context)
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            payment_info = payment.objects.filter(user=request.user).first()
+            product_cart = Cart.objects.filter(user=request.user).first()
+            pers_info = personal_info.objects.filter(user=request.user).first()
+
+    return render(request, 'checkout/review.html', {
+        'payment_info': payment_info,
+        'product_cart' :product_cart,
+        'pers_info': pers_info,
+    })
 
